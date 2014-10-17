f87697934658f709e31bba2d89da797319a0fd92
b1kjsh <joshuaman32@gmail.com>
2014-10-16 18:06:23 -0600

    initial commit

---
+++ UserScript/test_require.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       		Test_Require_Script
+// @namespace  		https://github.com/b1kjsh/
+// @version    		0.01
+// @grant       	none
+// @description		Master list of scripts
+// @copyright  		2014+, Joshua Howard
+// @include     	http://*
+// @require		   	https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
+// ==/UserScript==
+
+console.log("Loaded "+ GM_info.script.name +"version " + GM_info.script.version);
---
+++ masterlist.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       		MasterList
+// @namespace  		https://github.com/b1kjsh/
+// @version    		0.01
+// @grant       	none
+// @description		Master list of scripts
+// @copyright  		2014+, Joshua Howard
+// @include     	http://*
+// @require		   	https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
+// ==/UserScript==
+
+console.log("Loaded Master version " + GM_info.script.version);
